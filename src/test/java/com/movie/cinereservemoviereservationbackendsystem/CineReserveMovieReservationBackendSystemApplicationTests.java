b590f539192f3166ec477bf78540f7375bd710a1
gichinga03 <gichinga03@gmail.com>
2026-08-05 09:52:12 +0300

    first commit

---
+++ src/test/java/com/movie/cinereservemoviereservationbackendsystem/CineReserveMovieReservationBackendSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.cinereservemoviereservationbackendsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineReserveMovieReservationBackendSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
